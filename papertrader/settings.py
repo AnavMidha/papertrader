723572ed77f6fc17b1d62aa4a00fb93ee4389e7e
AnavMidha <anavmid@gmail.com>
2026-03-17 13:07:59 +0530

    fix: switch to psycopg3 for Python 3.14 compatibility

--- papertrader/settings.py
+++ papertrader/settings.py
@@ -87,4 +87,4 @@ FINNHUB_API_KEY = config('FINNHUB_API_KEY', default='')
 import dj_database_url
 DATABASE_URL = config('DATABASE_URL', default='')
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age=600, engine="django.db.backends.postgresql")
